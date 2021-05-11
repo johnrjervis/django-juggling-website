b152bba5ef97558120f1ed463133dcf2d6d8ef9c
johnrjervis <john_r_jervis@yahoo.co.uk>
2021-05-11 22:33:59 +0100

    Homepage shows video or error based on db contents

--- vlog/tests/tests.py
+++ vlog/tests/tests.py
@@ -15,13 +15,37 @@ class VideoModelTest(TestCase):
         response = self.client.get('/')
         self.assertIn('No videos are available!', response.content.decode())
 
+    def test_home_page_hides_video_element_if_no_videos_available(self):
+        response = self.client.get('/')
+
+        self.assertNotIn('</video>', response.content.decode())
+
+    def test_home_page_hides_error_if_videos_are_available(self):
+        first_video = JugglingVideo()
+        first_video.filename = 'behind_the_back_juggle.mp4'
+        first_video.save()
+
+        response = self.client.get('/')
+
+        self.assertNotIn('No videos are available!', response.content.decode())
+
+    def test_home_page_shows_video_if_availabe(self):
+        first_video = JugglingVideo()
+        first_video.filename = 'five_ball_juggle_50_catches.mp4'
+        first_video.save()
+
+        response = self.client.get('/')
+        #print(response.content.decode())
+
+        self.assertIn(first_video.filename, response.content.decode())
+
     def test_saving_and_retrieving_videos(self):
         first_video = JugglingVideo()
         first_video.filename = 'behind_the_back_juggle.mp4'
         first_video.save()
 
         saved_videos = JugglingVideo.objects.all()
-        self.assertEqual(saved_videos.count(), 1)
-
         first_saved_video = saved_videos[0]
+
+        self.assertEqual(saved_videos.count(), 1)
         self.assertEqual(first_video.filename, first_saved_video.filename)
--- vlog/views.py
+++ vlog/views.py
@@ -4,4 +4,16 @@ from vlog.models import JugglingVideo
 
 # Create your views here.
 def home_page(request):
-    return render(request, 'index.html')
+    videos = JugglingVideo.objects.all()
+    error_message = ''
+    first_video_filename = ''
+
+    if videos:
+        first_video_filename = videos[0].filename
+    else:
+        error_message = 'No videos are available!'
+
+    return render(request, 'index.html', {
+        'first_video' : first_video_filename,
+        'error_message': error_message
+    })
