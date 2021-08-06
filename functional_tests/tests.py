da1231fe283b279a089e87d9da82ee41d6d161cd
johnrjervis <john_r_jervis@yahoo.co.uk>
2021-08-06 22:43:57 +0100

    FT looks for link to video detail view

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -44,7 +44,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
         self.addCleanup(lambda: self.quit_if_possible(visitor_browser))
         self.browser.get(self.live_server_url)
 
-        # On inspecting the site's title, netizen realises that this is none other than JJ's juggling site
+        # On inspecting the site's title, the net user realises that this is none other than JJ's juggling site
         self.assertEqual("JJ's juggling site", self.browser.title)
 
         # The site's title element confirms it
@@ -56,6 +56,9 @@ class NewVisitorTest(StaticLiveServerTestCase):
         self.assertEqual('No videos are available!', error_message)
         videos = self.browser.find_elements_by_tag_name('video')
         self.assertEqual(len(videos), 0)
+        # There are also no links for further information on any videos
+        further_info_links = self.wait_for_element('info_link', self.browser.find_elements_by_class_name)
+        self.assertEqual(len(further_info_links), 0)
 
         # JJ logs in to the admin site and uploads a new video
         jj_browser = webdriver.Firefox()
@@ -74,16 +77,27 @@ class NewVisitorTest(StaticLiveServerTestCase):
         add_link.click()
         #time.sleep(4)
         new_video_field = self.wait_for_element('id_filename', self.browser.find_element_by_id)
-        new_video_field.send_keys('five_ball_juggle_50_catches.mp4')
+        first_video_filename = 'five_ball_juggle_50_catches.mp4'
+        new_video_field.send_keys(first_video_filename)
         #time.sleep(1)
         new_video_field.send_keys(Keys.ENTER)
         #time.sleep(4)
 
-        # On returning to the page after the update the net user sees a new video on the site
+        # On returning to the page after the update, the net user sees a new video on the site
         self.browser = visitor_browser
         self.browser.refresh()
         #time.sleep(4)
         videos = self.wait_for_element('video', self.browser.find_elements_by_tag_name)
         self.assertEqual(len(videos), 1)
-        self.assertIn('five_ball_juggle_50_catches.mp4', videos[0].get_attribute('innerHTML'))
+        self.assertIn(first_video_filename, videos[0].get_attribute('innerHTML'))
+        # The user notices that there is a link for more information about the video
+        further_info_link = self.wait_for_element('info_link', self.browser.find_element_by_class_name)
+        self.assertIn('Click here for more information on this video', further_info_link.text)
+        # The user clicks the link
+        further_info_link.click()
+        # The title of the video is displayed, although it seems to be just the video's filename
+        video_title = self.wait_for_element('detail_heading', self.browser.find_element_by_class_name)
+        self.assertEqual(first_video_filename, video_title.text)
+        # The format of the further info link is the base url + videos/ + a number with at least one digit
+        self.assertRegex(self.browser.current_url, r'/videos/\d+')
 
