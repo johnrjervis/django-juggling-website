bfcc709955cf0cd836c99916a4653e6ad4df3554
johnrjervis <john_r_jervis@yahoo.co.uk>
2021-08-09 17:30:54 +0100

    Refactor of unit tests

--- vlog/tests/tests.py
+++ vlog/tests/tests.py
@@ -4,27 +4,27 @@ from vlog.models import JugglingVideo
 
 class HomePageViewTest(TestCase):
 
-    def test_home_page_returns_correct_html(self):
+    def test_home_page_uses_correct_template(self):
         response = self.client.get('/')
+
         self.assertTemplateUsed(response, 'vlog/index.html')
 
     def test_home_page_displays_error_if_no_videos_in_database(self):
         response = self.client.get('/')
-        self.assertIn('No videos are available!', response.content.decode())
+
+        self.assertContains(response, 'No videos are available!')
 
     def test_home_page_hides_video_element_if_no_videos_available(self):
         response = self.client.get('/')
 
-        self.assertNotIn('</video>', response.content.decode())
+        self.assertNotContains(response, '</video>')
 
     def test_home_page_hides_error_if_videos_are_available(self):
-        first_video = JugglingVideo()
-        first_video.filename = 'behind_the_back_juggle.mp4'
-        first_video.save()
+        first_video = JugglingVideo.objects.create(filename = 'behind_the_back_juggle.mp4')
 
         response = self.client.get('/')
 
-        self.assertNotIn('No videos are available!', response.content.decode())
+        self.assertNotContains(response, 'No videos are available!')
 
     def test_home_page_shows_video_if_availabe(self):
         first_video = JugglingVideo()
@@ -34,26 +34,26 @@ class HomePageViewTest(TestCase):
         response = self.client.get('/')
         #print(response.content.decode())
 
-        self.assertIn(first_video.filename, response.content.decode())
+        self.assertContains(response, first_video.filename)
 
 class VideoDetailViewTest(TestCase):
 
+    def test_detail_view_uses_correct_template(self):
+        response = self.client.get('/videos/1/')
+
+        self.assertTemplateUsed(response, 'vlog/detail.html')
+
     def test_detail_view_displays_video(self):
-        first_video = JugglingVideo()
-        first_video.filename = 'five_ball_juggle_50_catches.mp4'
-        first_video.save()
+        first_video = JugglingVideo.objects.create(filename = 'five_ball_juggle_50_catches.mp4')
 
         response = self.client.get('/videos/1/')
 
         self.assertContains(response, first_video.filename)
-        self.assertTemplateUsed(response, 'vlog/detail.html')
 
 class VideoModelTest(TestCase):
 
     def test_saving_and_retrieving_videos(self):
-        first_video = JugglingVideo()
-        first_video.filename = 'behind_the_back_juggle.mp4'
-        first_video.save()
+        first_video = JugglingVideo.objects.create(filename = 'behind_the_back_juggle.mp4')
 
         saved_videos = JugglingVideo.objects.all()
         first_saved_video = saved_videos[0]
