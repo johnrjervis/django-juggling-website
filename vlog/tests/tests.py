fea78696588f16f7ed03d0b6e064d152d504dad0
johnrjervis <john_r_jervis@yahoo.co.uk>
2021-08-14 20:11:23 +0100

    Videos not published before their pub. date

--- vlog/tests/tests.py
+++ vlog/tests/tests.py
@@ -37,14 +37,28 @@ class IndexViewTest(TestCase):
 
     def test_home_page_shows_most_recently_published_video(self):
         older_date = timezone.now() - timedelta(days = 5)
-        newer_date = timezone.now()
+        current_date = timezone.now()
         older_video = JugglingVideo.objects.create(filename = 'behind_the_back_juggle.mp4', pub_date = older_date)
-        newer_video = JugglingVideo.objects.create(filename = 'five_ball_juggle_50_catches.mp4', pub_date = newer_date)
+        current_video = JugglingVideo.objects.create(filename = 'five_ball_juggle_50_catches.mp4', pub_date = current_date)
 
         response = self.client.get('/juggling/')
 
-        self.assertContains(response, newer_video.filename)
+        self.assertContains(response, current_video.filename)
         self.assertNotContains(response, older_video.filename)
+
+    def test_home_page_does_not_show_videos_that_have_a_publication_date_in_the_future(self):
+        """
+        Videos with a publication date in the future should not appear on the homepage
+        """
+        future_date = timezone.now() + timedelta(days = 5)
+        current_date = timezone.now()
+        future_video = JugglingVideo.objects.create(filename = 'behind_the_back_juggle.mp4', pub_date = future_date)
+        current_video = JugglingVideo.objects.create(filename = 'five_ball_juggle_50_catches.mp4', pub_date = current_date)
+
+        response = self.client.get('/juggling/')
+
+        self.assertContains(response, current_video.filename)
+        self.assertNotContains(response, future_video.filename)
 
 class VideoDetailViewTest(TestCase):
 
@@ -91,6 +105,18 @@ class VideoDetailViewTest(TestCase):
 
         self.assertContains(response, time_string)
 
+    def test_detail_view_unavailable_for_videos_with_a_pub_date_in_the_future(self):
+        """
+        The URL for a video object with a publication date in the future should respond with a 404 error
+        """
+        future_date = timezone.now() + timedelta(days = 5)
+        future_video = JugglingVideo.objects.create(filename = 'five_ball_juggle_50_catches.mp4', pub_date = future_date)
+
+        response = self.client.get(f'/juggling/videos/{future_video.id}/')
+
+        self.assertEqual(response.status_code, 404)
+
+
 class VideosListViewTest(TestCase):
 
     def test_video_list_view_uses_correct_template(self):
@@ -113,22 +139,57 @@ class VideosListViewTest(TestCase):
         self.assertContains(response, older_video.filename)
         self.assertNotContains(response, newer_video.filename)
 
-    def test_newer_videos_displayed_first_in_videos_archive(self):
+    def test_videos_delivered_in_order_of_increasing_age_in_context(self):
         """
-        Videos in the archive should be displayed newest first
+        Videos in the archive should be provided to the HTML in increasing age oder so that newer videos appear first on the page
         """
         oldest_date = timezone.now() - timedelta(days = 10)
         older_date = timezone.now() - timedelta(days = 5)
-        newer_date = timezone.now()
+        current_date = timezone.now()
         oldest_video = JugglingVideo.objects.create(filename = 'under_the_arm.mp4', pub_date = oldest_date)
         older_video = JugglingVideo.objects.create(filename = 'behind_the_back_juggle.mp4', pub_date = older_date)
-        newer_video = JugglingVideo.objects.create(filename = 'five_ball_juggle_50_catches.mp4', pub_date = newer_date)
+        current_video = JugglingVideo.objects.create(filename = 'five_ball_juggle_50_catches.mp4', pub_date = current_date)
 
         response = self.client.get('/juggling/videos/')
 
         self.assertEqual(len(response.context['videos_list']), 2)
         self.assertEqual(response.context['videos_list'][0], older_video)
         self.assertEqual(response.context['videos_list'][1], oldest_video)
+
+    def test_videos_page_displays_multiple_videos(self):
+        """
+        The archive should display all videos published before the latest video
+        """
+        oldest_date = timezone.now() - timedelta(days = 10)
+        older_date = timezone.now() - timedelta(days = 5)
+        current_date = timezone.now()
+        oldest_video = JugglingVideo.objects.create(filename = 'under_the_arm.mp4', pub_date = oldest_date)
+        older_video = JugglingVideo.objects.create(filename = 'behind_the_back_juggle.mp4', pub_date = older_date)
+        current_video = JugglingVideo.objects.create(filename = 'five_ball_juggle_50_catches.mp4', pub_date = current_date)
+
+        response = self.client.get('/juggling/videos/')
+
+        self.assertContains(response, older_video.filename)
+        self.assertContains(response, oldest_video.filename)
+
+    def test_videos_page_not_affected_by_videos_with_a_publication_date_in_the_future(self):
+        """
+        The videos page should ignore videos with a publication date in the future
+        Of the rest of the videos, the most recent (non-future) video should appear on the home page
+        The rest should appear on the videos page
+        """
+        future_date = timezone.now() + timedelta(days = 5)
+        older_date = timezone.now() - timedelta(days = 5)
+        current_date = timezone.now()
+        future_video = JugglingVideo.objects.create(filename = 'behind_the_back_juggle.mp4', pub_date = future_date)
+        older_video = JugglingVideo.objects.create(filename = 'five_ball_juggle_50_catches.mp4', pub_date = older_date)
+        current_video = JugglingVideo.objects.create(filename = 'under_the_arm.mp4', pub_date = current_date)
+
+        response = self.client.get('/juggling/videos/')
+
+        self.assertContains(response, older_video.filename)
+        self.assertNotContains(response, current_video.filename)
+        self.assertNotContains(response, future_video.filename)
 
 class VideoModelTest(TestCase):
 
--- vlog/views.py
+++ vlog/views.py
@@ -1,11 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 #from django.http import HttpResponse
 from vlog.models import JugglingVideo
+from django.utils import timezone
 
 # Create your views here.
 
 def index(request):
-    videos = JugglingVideo.objects.all().order_by('-pub_date')
+    videos = JugglingVideo.objects.filter(pub_date__lte = timezone.now()).order_by('-pub_date')
     latest_video = ''
 
     if videos:
@@ -16,7 +17,7 @@ def index(request):
     })
 
 def videos(request):
-    videos = JugglingVideo.objects.all().order_by('-pub_date')
+    videos = JugglingVideo.objects.filter(pub_date__lte = timezone.now()).order_by('-pub_date')
     videos_list = ''
 
     if len(videos) > 1:
@@ -27,7 +28,7 @@ def videos(request):
     })
 
 def detail(request, jugglingvideo_id):
-    video = JugglingVideo.objects.get(id = jugglingvideo_id)
+    video = get_object_or_404(JugglingVideo.objects.filter(pub_date__lte = timezone.now()), id = jugglingvideo_id)
 
     return render(request, 'vlog/detail.html', {
         'video' : video,
