d30c3fa1ebe9f789c7940b829beb7fdf21b5c90d
johnrjervis <john_r_jervis@yahoo.co.uk>
2021-09-02 16:41:41 +0100

    Added str() and repr() to JV model

--- vlog/models.py
+++ vlog/models.py
@@ -7,3 +7,9 @@ class JugglingVideo(models.Model):
     filename = models.CharField(max_length = 60, default = '')
     title = models.CharField(max_length = 50, default = '')
     pub_date = models.DateTimeField(default = timezone.now)
+
+    def __str__(self):
+        return f'Juggling video: {self.filename}'
+
+    def __repr__(self):
+        return f'{self.__class__}: {self.filename}'
