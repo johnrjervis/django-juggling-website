03754f041920ae7d8df08c29491aff9c8780927b
johnrjervis <john_r_jervis@yahoo.co.uk>
2022-01-20 22:27:05 +0000

    Re-styling of comments with author name

--- vlog/admin.py
+++ vlog/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import JugglingVideo
+from .models import JugglingVideo, VideoComment
 
 # Register your models here.
 admin.site.register(JugglingVideo)
+admin.site.register(VideoComment)
