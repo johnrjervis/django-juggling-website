a640006db833e253e15a3276d4352c83f6b65e22
johnrjervis <john_r_jervis@yahoo.co.uk>
2022-03-07 22:38:31 +0000

    Style updates for dev pages

--- dev/tests/tests.py
+++ dev/tests/tests.py
@@ -1,9 +1,10 @@
 from django.test import TestCase
 from django.urls import reverse
+from vlog.tests.base import JugglingVideoSiteTest
 
 # Create your tests here.
 
-class IndexViewTest(TestCase):
+class ProgrammingViewTest(JugglingVideoSiteTest):
     """
     Tests for the programming page
     """
@@ -15,3 +16,10 @@ class IndexViewTest(TestCase):
         response = self.client.get(reverse('dev:programming'))
 
         self.assertTemplateUsed(response, 'dev/programming.html')
+
+    def test_context_dict_contains_correct_selected_item_for_index_view(self):
+        """
+        The context dict for the home page view should contain 'selected': 'Programming'
+        The selected class should be present on the programming page
+        """
+        self.check_context_dict_contains_correct_selected_item_for_view('dev:programming', 'Programming')
--- dev/views.py
+++ dev/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def programming(request):
-    return render(request, 'dev/programming.html')
+    return render(request, 'dev/programming.html', {'selected': 'Programming'})
